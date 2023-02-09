b39bdc0ae1cb18bd1203b66d56cd703163de20a9
Ed Zimny <ebzimny@gmail.com>
2023-02-08 20:31:55 -0500

    added comments and changed green plus icon size

--- scripts/box-score-analysis.js
+++ scripts/box-score-analysis.js
@@ -1,15 +1,15 @@
-/** Goals for this extension are as follows:
+/** Goals for this enhancement are as follows:
  * 1. Create a link to the team's GDAnalyst game analysis schedule page.
  * 2. Create individual analysis links next to each box score link.
  * 3. Do 1 and 2 for main schedule page.
  * 4. Do 1 and 2 for team profile page schedule tab.
- * 5. Do 1 and 2 for team profile page gamelog tab.
  */
 
 let url = window.location.href;
 const main_schedule_page = 'https://www.whatifsports.com/gd/schedule';
 const teamId_schedule_page = 'https://www.whatifsports.com/gd/TeamProfile/Schedule.aspx?tid=';
 const teamId_gamelog_page = 'https://www.whatifsports.com/gd/TeamProfile/GameLog.aspx?tid=';
+// Get the active teamId
 const active_school_id = document.getElementById('pagetid')['value'];
 const gdanalyst_team_schedule_page = `https://gdanalyst.herokuapp.com/${active_school_id}/schedule`;
 const parser = new DOMParser();
@@ -27,7 +27,14 @@ if (url.startsWith(main_schedule_page) || url.startsWith(teamId_schedule_page)) 
 
     // gets all cells with box score link
     let r = d1[0].querySelectorAll("a.boxscoreLink");
+    // regex that finds each boxscore ID
     let regex = /OpenBoxscore\((\d{7})\)/;
+
+    /** Iterates through each row element to add plus icon and box score analysis
+     * analysis URL in front of each box score link.
+     * When the user clicks on the plus icon, it will open GDAnalyst website
+     * and analyze that specific boxscore.
+     */
     r.forEach(element => {
         element.setAttribute('style','vertical-align:middle;');
         let boxscoreid = element.getAttribute('href').match(regex);
@@ -37,7 +44,7 @@ if (url.startsWith(main_schedule_page) || url.startsWith(teamId_schedule_page)) 
         newlink.setAttribute('target',"_blank");
         let imagefile = 'images/plus-icon.png';
         let imageurl = chrome.runtime.getURL(imagefile);
-        let html_to_insert = parser.parseFromString(`<img src="${imageurl}" height="16px" width="16px" style="vertical-align:middle; margin-right:3px"></img>`, "text/html");
+        let html_to_insert = parser.parseFromString(`<img src="${imageurl}" height="12px" width="12px" style="vertical-align:middle; margin-right:3px"></img>`, "text/html");
         newlink.appendChild(html_to_insert.body.firstChild);
         element.insertAdjacentElement("beforebegin", newlink);
     })
